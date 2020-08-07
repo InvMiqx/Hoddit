ecb95c8ed876aa8a969fccedebc0759541f3f971
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-07 03:42:55 -0400

    a

---
+++ src/components/RedditPosts.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Postrow from './Postrow';
+
+const RedditPosts = () => {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(async () =>{
+
+
+    const result = await fetch("https://cors-anywhere.herokuapp.com/https://hoddit-api.herokuapp.com/api/p?limit=100", {
+      origin: "a",
+      method: 'GET',
+      headers: {'Content-Type':'application/json'},
+     });
+
+    // setData(result);
+    // console.log(result);
+    console.log(result);
+  }, []);
+
+  return (
+    // <ul>
+    //   {data.hits.map(item => (
+    //     <li key={item.title}>
+    //       {item.title}
+    //     </li>
+    //   ))}
+    // </ul>
+
+    <div></div>
+  );
+};
+
+export default RedditPosts;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../images/logo2.png';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img className="logo" src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default Header;
