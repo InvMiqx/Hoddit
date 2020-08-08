fc31b7f673974abf4529b7d2b66fe27f45d3d9b7
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-07 21:54:02 -0400

    a

--- src/components/RedditPosts.js
+++ src/components/RedditPosts.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Postrow from './Postrow';
 
 const RedditPosts = () => {
-  // const [data, setData] = useState("");
-  //
-  // useEffect(async () =>{
-  //
-  //   // const result = JSON.parse(JSON.stringify({"_id":"5f279f3d0ef27d001775bd74","ups":141670,"title":"Who knows why?","created_utc":1596391948,"url":"https://i.redd.it/8l82z1uarme51.png","subreddit":"memes","rate":211.2270761890562,"flipper":false},{"_id":"5f279f3d0ef27d001775c0fa","ups":156527,"title":"Fuck Nepotism","created_utc":1596381721,"url":"https://i.imgur.com/JEu4eZS.gifv","subreddit":"dankmemes","rate":186.08690483266957,"flipper":false}));
-  //   // setData(result);
-  //
-  // }, []);
+  const [data, setData] = useState("");
+
+  useEffect(async () =>{
+
+    const result = await fetch("/api/query?limit=100");
+    const result_json = await result.json();
+
+    setData(
+      result_json.map(renderIndividualRow())
+    );
+
+    // const result = JSON.parse(JSON.stringify({"_id":"5f279f3d0ef27d001775bd74","ups":141670,"title":"Who knows why?","created_utc":1596391948,"url":"https://i.redd.it/8l82z1uarme51.png","subreddit":"memes","rate":211.2270761890562,"flipper":false},{"_id":"5f279f3d0ef27d001775c0fa","ups":156527,"title":"Fuck Nepotism","created_utc":1596381721,"url":"https://i.imgur.com/JEu4eZS.gifv","subreddit":"dankmemes","rate":186.08690483266957,"flipper":false}));
+    // setData(result);
+
+  }, []);
 
   const renderIndividualRow = (post) => {
     return <Postrow
@@ -25,11 +32,11 @@ const RedditPosts = () => {
 
   const renderCustomRows = async () => {
 
-    const result = await fetch("/api/query?limit=100");
-    const result_json = await result.json();
+    // const result = await fetch("/api/query?limit=100");
+    // const result_json = await result.json();
     // setData(result_json);
 
-    console.log(result_json);
+    // console.log(result_json);
 
     // let postInfo;
     // let dataTemp = JSON.stringify(data);
@@ -48,7 +55,7 @@ const RedditPosts = () => {
   return (
     <ul>
 
-      {renderCustomRows()}
+      {data}
     </ul>
   );
 };
