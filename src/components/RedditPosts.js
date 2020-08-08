0f0172f5d2e3174096baf8ebbed2411d4dc300c7
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-07 21:33:20 -0400

    a

--- src/components/RedditPosts.js
+++ src/components/RedditPosts.js
@@ -28,6 +28,7 @@ const RedditPosts = () => {
 
   const renderCustomRows = () => {
 
+    console.log(data[0]);
 
     // let postInfo;
     // let dataTemp = JSON.stringify(data);
@@ -39,8 +40,8 @@ const RedditPosts = () => {
     //
     // return postInfo;
 
-    let redditData = data.map(renderIndividualRow());
-    return redditData;
+    // let redditData = data.map(renderIndividualRow());
+    // return redditData;
   }
 
   return (
