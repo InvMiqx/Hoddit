3a47a744de5eb2c1a3badb42d7ae56d03721bd22
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-09 20:54:28 -0400

    a

---
+++ src/components/MainArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainArea = () => {
+  return (
+    <div className="mainTextArea">
+      <h3>Hoddit</h3>
+      <h4>The fastest growing reddit posts, right now</h4>
+    </div>
+  );
+};
+
+export default MainArea;
