912f1dacdf89d04562745b980ba20af33c168e6f
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-25 18:38:56 -0400

    a

--- server.js
+++ server.js
@@ -244,18 +244,19 @@ async function poster(flipper){
           let posts = [];
 
           dataset.forEach(data => {
-            data.children.forEach(post =>{
-
-              posts.push({
-                "ups": post.data.ups,
-                "title": post.data.title,
-                "created_utc": post.data.created_utc,
-                "url": "http://www.reddit.com" + post.data.permalink,
-                "subreddit": post.data.subreddit,
-                "rate": post.data.ups/((Math.round((new Date().getTime()/1000)-post.data.created_utc)/60)),
-                "flipper": flipper
-              });
-            });
+            console.log("fuck my ass");
+            // data.children.forEach(post =>{
+            //
+            //   posts.push({
+            //     "ups": post.data.ups,
+            //     "title": post.data.title,
+            //     "created_utc": post.data.created_utc,
+            //     "url": "http://www.reddit.com" + post.data.permalink,
+            //     "subreddit": post.data.subreddit,
+            //     "rate": post.data.ups/((Math.round((new Date().getTime()/1000)-post.data.created_utc)/60)),
+            //     "flipper": flipper
+            //   });
+            // });
           });
 
           post.collection.insertMany(posts, function(err, docs){
