5962cff5f61623861bf0498c3c91fdb1445614a7
InvMiqx <39607713+InvMiqx@users.noreply.github.com>
2020-08-25 20:09:55 -0400

    a

--- server.js
+++ server.js
@@ -243,7 +243,7 @@ async function poster(flipper){
 
           let posts = [];
           dataset.forEach(data => {
-            console.log(data);
+            console.log(data.data);
             // console.log(data.data.children);
             // data.data.children.forEach(post =>{
             //   posts.push({
